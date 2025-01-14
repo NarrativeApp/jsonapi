cb8e83bf83617a3cf5fad90405b7f3038007aa5b
Paul Oliver <puzza007@gmail.com>
2025-01-14 20:00:22 +1300

    fix(config): update mix_project to JSONAPI.Mixfile in config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :git_ops,
-  mix_project: Conduit.MixProject,
+  mix_project: JSONAPI.Mixfile,
   changelog_file: "CHANGELOG.md",
   repository_url: "https://github.com/NarrativeApp/jsonapi",
   manage_mix_version?: true,
